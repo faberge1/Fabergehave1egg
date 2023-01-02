59120b86798fe5290392b52e5e592cbc04509b23
faberge1 <115358705+faberge1@users.noreply.github.com>
2023-01-02 21:29:36 +0300

    Add files via upload

---
+++ v-catalog-item.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='v-catalog-item'>  
+    <p>Item 1</p>
+    <p>Price: 100</p>
+    <button>add to cart</button>
+    </div>
+    
+    </template>
+    
+    <script>
+    
+    export default{
+        name: "v-catalog-item",
+        props:{},
+        data() {
+        return {}
+        },
+        computed: {}
+    }
+    </script>
+
+    <style>
+
+
+    </style>
---
+++ v-main-wrapper.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="v-main-wrapper">
+    <p>{{title}}</p>
+    <v-catalog />
+    <v-cart />
+
+
+</div>
+
+</template>
+
+<script>
+import Vcatalog from './components/v-catalog';
+import Vcart from './v-cart';
+  export default{
+    name: 'v-main-wrapper',
+    components: {
+        vCatalog,
+        vCart
+    },
+    props:{},
+    data() {
+    return{
+    title: 'Main Wrapper'
+        }
+    },
+    computed: {},
+    methods: {},
+    watch: {},
+    mounted() {
+        console.log('Hello i am alive!')
+    }
+
+
+
+}
+
+
+</script>
+
+<style>
+.v-main-wrapper{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 900px;
+    margin: 0 auto;
+
+}
+</style>
